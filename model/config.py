bf3a8f7ce1da3c16d8aa77290291b6fbabf7de5a
Siddhant Jain <siddhantjain209@gmail.com>
2024-06-09 21:10:29 +0530

    change max_output_tokens to 1000

--- model/config.py
+++ model/config.py
@@ -3,6 +3,6 @@ MODEL_NAME = "gemini-1.5-flash"
 GENERATION_CONFIG = {
             "temperature": 0.2,
             "top_p": 0.95,
-            "max_output_tokens": 400,
+            "max_output_tokens": 1000,
             "response_mime_type": "text/plain",
         }
