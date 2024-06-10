92c58cafca1e6aca9a40ef80d266da0e0bacb6ef
Siddhant Jain <siddhantjain209@gmail.com>
2024-06-10 16:12:00 +0530

    add /search in perplexed_bot

--- discord_bots/perplexed_bot.py
+++ discord_bots/perplexed_bot.py
@@ -1,11 +1,13 @@
 import discord
 from model.model import *
+from utils.reddit import Reddit
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 model = Model()
+reddit = Reddit()
 
 @client.event
 async def on_ready():
@@ -21,6 +23,15 @@ async def on_message(message):
             question = message.content.split('/question ')[1]
             processing_msg = await message.channel.send("Processing your question...")
             response = model.generate(question)
+            await processing_msg.edit(content=response)
+        except Exception as e:
+            await message.channel.send("Some error occurred, unable to process...", e)
+    
+    if message.content.startswith("/search"):
+        try:
+            question = message.content.split('/search ')[1].strip()
+            processing_msg = await message.channel.send("Processing your question...")
+            response = reddit.get_subreddit_urls(question)
             await processing_msg.edit(content=response)
         except Exception as e:
             await message.channel.send("Some error occurred, unable to process...", e)
